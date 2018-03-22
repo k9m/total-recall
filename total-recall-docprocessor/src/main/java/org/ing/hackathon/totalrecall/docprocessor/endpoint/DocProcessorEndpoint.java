490748e28719883ccf5181221c0b9d97b5f2d6bb
Bohdan Tsymbala <Bohdan.Tsymbala@ing.com>
2018-03-22 02:23:28 +0100

    Added cross origin policy annotations.

--- total-recall-docprocessor/src/main/java/org/ing/hackathon/totalrecall/docprocessor/endpoint/DocProcessorEndpoint.java
+++ total-recall-docprocessor/src/main/java/org/ing/hackathon/totalrecall/docprocessor/endpoint/DocProcessorEndpoint.java
@@ -6,6 +6,7 @@ import org.ing.hackathon.totalrecall.docprocessor.model.docprocessor.Document;
 import org.ing.hackathon.totalrecall.docprocessor.repo.DocumentRepository;
 import org.ing.hackathon.totalrecall.docprocessor.service.PdfToImageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class DocProcessorEndpoint {
 
   @Autowired
--- total-recall-docprocessor/src/main/java/org/ing/hackathon/totalrecall/docprocessor/repo/DocumentRepository.java
+++ total-recall-docprocessor/src/main/java/org/ing/hackathon/totalrecall/docprocessor/repo/DocumentRepository.java
@@ -3,7 +3,9 @@ package org.ing.hackathon.totalrecall.docprocessor.repo;
 import org.ing.hackathon.totalrecall.docprocessor.model.docprocessor.Document;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(path = "documents", collectionResourceRel = "documents")
+@CrossOrigin(origins = "http://localhost:4200")
 public interface DocumentRepository extends PagingAndSortingRepository<Document, String> {
 }
