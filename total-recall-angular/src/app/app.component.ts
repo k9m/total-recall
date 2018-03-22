3f526106f1c3b5adf148863fa5bd376e3b35656a
Bohdan Tsymbala <Bohdan.Tsymbala@ing.com>
2018-03-22 10:13:09 +0100

    Moved JSON stringifying to Typescript.

--- total-recall-angular/src/app/app.component.ts
+++ total-recall-angular/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
     documents: Array<Document>;
     currentDocument: Document;
     currentPage: DocumentPage;
-    savedDocumentsData: Map<string, Object> = new Map();
+    savedDocumentsData: Map<string, string> = new Map();
 
     constructor(private documentsService: DocumentsService) {}
 
@@ -114,7 +114,7 @@ export class AppComponent implements OnInit {
                         page => page.changed = false
                     );
                     this.documentsService.getDocumentData(document.documentType, document.documentId).subscribe(data => {
-                        this.savedDocumentsData.set(document.documentId, data);
+                        this.savedDocumentsData.set(document.documentId, JSON.stringify(data));
                     });
                 });
         }
